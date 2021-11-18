36cdf5f27f64abd140366d39f8a3a1b5b3130b35
Ken883 <68206257+Ken883@users.noreply.github.com>
2021-11-17 19:40:19 -0800

    Add files via upload

---
+++ mvp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	char space[50];
+	printf(" Search :");
+	scanf("%s",&space);
+
+	printf("%s:relating to england or its people or language\n",space);
+
+	return 0;
+}
